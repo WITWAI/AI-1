0b2ef53754db15b5a2eec08a237aebead6f30705
WITWAI <XMA024@e.ntu.edu.sg>
2024-11-13 03:49:23 +0000

    1

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-from flash
+from flask import Flask
+from flask import render_template,request
+
+app = flask(__name__)
+
+@app.route("/",methods=["GET","POST"])
+def index():
+    return(render_template("index.html"))
+
+if __name__ == "__main__":
+    app.run()
